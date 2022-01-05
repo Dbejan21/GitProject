ed7d5fba02fbd798f3d0bd527f67b5227cd9439f
DianaSDET <udiana999@gmail.com>
2022-01-04 18:40:23 -0600

    forth commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,5 +3,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("first");
         System.out.println("second");
+        System.out.println("third");
+        System.out.println("forth");
     }
 }
